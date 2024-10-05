779ea842f15629c59313e18663072d11d925aec6
Abitha <148053196+Abitha1111@users.noreply.github.com>
2024-10-05 19:21:47 +0530

    Update ServerWebSocket.java

--- com/ServerWebSocket.java
+++ com/ServerWebSocket.java
@@ -5,8 +5,7 @@ import javax.websocket.OnError;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
-import javax.websocket.server.ServerEndpoint;
-
+import javax.websocket.server.ServerEndpoin
 @ServerEndpoint("/serverws")
 public class ServerWebSocket {
 
