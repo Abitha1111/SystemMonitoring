d597c75a05444227e04d9665c5d93877525c315c
Abitha <148053196+Abitha1111@users.noreply.github.com>
2024-10-07 11:25:16 +0530

    Delete eclipse-workspace directory

--- eclipse-workspace/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/CakeShop/style.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.querySelectorAll('.cake-slider').forEach(function(slider) {
-      slider.addEventListener('wheel', function(event) {
-        event.preventDefault();
-        slider.scrollLeft += event.deltaY;
-      });
-    });
-  });
-  
