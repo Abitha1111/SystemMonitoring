6a2cb26228329f33ebe1cb23205b0b378af94c25
praveenkanna25 <131360166+praveenkanna25@users.noreply.github.com>
2024-10-09 18:58:42 +0530

    Update SideMenu.vue

--- SideMenu.vue
+++ SideMenu.vue
@@ -1,5 +1,4 @@
-
- <template>
+<template>
   <div class="side-menu">
     <h1 class="title">System Health Monitor</h1>
     <ul>
@@ -18,15 +17,12 @@
         <!-- Dropdown list, shown when `showDropdown` is true -->
         <ul v-show="showDropdown" class="dropdown">
           <li v-for="(systemInfo, index) in systemData" :key="index"
-    :class="{ 'active-item': activeMenu === `system-${index}` }"
-    @click.stop="setActiveMenu(`system-${index}`)">
- 
-    <!-- Ensure you're dynamically passing the system name into the router link -->
-  <router-link :to="{ name: 'DrawerTable', params: { systemName: systemInfo }}" exact>
-  {{ systemInfo }}
-</router-link>
-
-</li>
+              :class="{ 'active-item': activeMenu === `system-${index}` }"
+              @click.stop="setActiveMenu(`system-${index}`)">
+            <router-link :to="{ name: 'DrawerTable', params: { systemName: systemInfo }}" exact>
+              {{ systemInfo }}
+            </router-link>
+          </li>
         </ul>
       </li>
     </ul>
@@ -40,15 +36,16 @@ export default {
       activeMenu: '',
       showDropdown: false,
       systemData: [
-        'viji_info', 
-        'Abitha_info',
-        'Kirthiga_info',
-        'Thulasi_info',
-        'Thangavi_info', 
-        'Pragathi_info', 
-        'praveen_info', 
-        'mounish_info',
-        'Swathi_info'
+        'Abitha_Info', 
+        'Hari_Info',
+        'Kirthiga_Info',
+        'Mounesh_Info',
+        'Pragathi_Info', 
+        'Praveen_Info', 
+        'Swathi_Info', 
+        'Thangavi_Info',
+        'Thulasi_Info',
+        'Viji_Info'
       ]
     };
   },
@@ -65,7 +62,6 @@ export default {
 };
 </script>
 
-
 <style scoped>
 .side-menu {
   width: 250px;
@@ -77,31 +73,24 @@ export default {
   left: 0;
   top: 0;
   z-index: 100;
- 
 }
-h1.title{
+h1.title {
   font-size: 20px;
 }
-
-
 .side-menu h1 {
-  widows: 100%;
+  width: 100%;
   margin-top: 0;
   font-size: 23px;
   margin-bottom: 50px;
 }
-
 .side-menu ul {
   list-style: none;
   padding: 0;
 }
-
 .side-menu li {
   margin: 15px 0;
   font-size: large;
-
 }
-
 .side-menu a {
   color: white;
   font-size: 20px;
@@ -110,38 +99,24 @@ h1.title{
   padding: 10px 20px; /* Add left padding to align text inside */
   margin: 0;
 }
-
 a.side-menu1 {
-  /* Removed flex to allow natural block-level alignment */
   padding: 10px 20px;
   display: block;
   text-align: left; /* Align text to the left */
 }
-
 .active-item a {
+  border-radius: 10px;
   background-color: rgb(202, 55, 55);
 }
-
 .dropdown {
   list-style: none;
   padding-left: 20px;
-  /* background-color: #140244; */
 }
-
 .dropdown li {
   margin: 10px 0;
-  background-color: transparent;
 }
-
 .dropdown a {
   color: white;
   padding-left: 40px; /* Indent the dropdown items for a better hierarchy */
-}
-a.side-menu1[data-v-79f86886] {
-    padding: 10px 20px;
-    display: block;
-    text-align: center;
-    font-size: 25px;
-    font-family:Georgia, 'Times New Roman';
 }
 </style>
